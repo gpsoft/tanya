090380643686c7fed57c131b8e6a7536e4942505
gpsoft <etoh.gpsoft@gmail.com>
2018-01-04 18:19:38 +0900

    Switch between two agents by profile

--- src/main/java/jp/dip/gpsoft/tanya/AppConfig.java
+++ src/main/java/jp/dip/gpsoft/tanya/AppConfig.java
@@ -2,16 +2,19 @@ package jp.dip.gpsoft.tanya;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
+	@Profile("use_normalagent")
 	public IAgent agent() {
 		return new Agent();
 	}
 
 	@Bean
+	@Profile("use_specialagent")
 	public IAgent specialAgent() {
 		return new SpecialAgent();
 	}
