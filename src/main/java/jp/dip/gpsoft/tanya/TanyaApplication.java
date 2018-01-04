e0ae5324452b6c1c70bcdc0d9ca32f3658110a17
gpsoft <etoh.gpsoft@gmail.com>
2018-01-04 18:02:17 +0900

    Hello world

--- src/main/java/jp/dip/gpsoft/tanya/TanyaApplication.java
+++ src/main/java/jp/dip/gpsoft/tanya/TanyaApplication.java
@@ -1,12 +1,25 @@
 package jp.dip.gpsoft.tanya;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TanyaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TanyaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			test();
+		};
+	}
+
+	private void test() {
+		System.out.println("Hello, world!");
 	}
 }
