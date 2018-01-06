89bc7194b1538444bd74bbad5c55ceba6bbef48e
gpsoft <etoh.gpsoft@gmail.com>
2018-01-06 13:03:49 +0900

    Add api for cats

--- src/main/java/jp/dip/gpsoft/tanya/controller/ApiController.java
+++ src/main/java/jp/dip/gpsoft/tanya/controller/ApiController.java
@@ -10,8 +10,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
+	class Cat {
+		public String name;
+		public Integer age;
+
+		public Cat(String name, Integer age) {
+			this.name = name;
+			this.age = age;
+		}
+	}
+
+	private List<Cat> cats;
+
+	{
+		cats = Arrays.asList(
+				new Cat("クーちゃん", 12),
+				new Cat("ターちゃん", 11));
+	}
+
 	@RequestMapping("/fruits")
 	public List<String> func1() {
 		return Arrays.asList("apple", "orange", "banana");
+	}
+
+	@RequestMapping("/cats")
+	public List<Cat> func2() {
+		return cats;
+	}
+
+	@RequestMapping("/cats/1")
+	public Cat func3() {
+		return cats.get(0);
 	}
 }
