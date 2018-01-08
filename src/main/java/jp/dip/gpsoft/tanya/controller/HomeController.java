397faac03dcfdae684007785d5fb9bc54ccea64c
gpsoft <etoh.gpsoft@gmail.com>
2018-01-08 17:33:56 +0900

    Use message file

--- src/main/java/jp/dip/gpsoft/tanya/controller/HomeController.java
+++ src/main/java/jp/dip/gpsoft/tanya/controller/HomeController.java
@@ -1,7 +1,10 @@
 package jp.dip.gpsoft.tanya.controller;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,11 +20,15 @@ public class HomeController {
 	@Autowired
 	private Logger logger;
 
+	@Autowired
+	private MessageSource msgSource;
+
 	@RequestMapping("/home")
 	@ResponseBody
-	public String home() {
+	public String home(Locale locale) {
 		logger.info("/home");
-		return "<h2>Home</h2><p>Welcome!!</p>";
+		String msg = msgSource.getMessage("msg.introduction", null, locale);
+		return "<h2>Home</h2><p>" + msg + "</p>";
 	}
 
 	@RequestMapping("/")
@@ -34,20 +41,20 @@ public class HomeController {
 	public String greet(@PathVariable String name) {
 		return "<p>Hello " + name + "</p>";
 	}
-	
+
 	@RequestMapping("/greet2")
 	@ResponseBody
-	public String greet2(@RequestParam(value="name",defaultValue="") String name) {
-		if ( name.equals("") ) {
+	public String greet2(@RequestParam(value = "name", defaultValue = "") String name) {
+		if (name.equals("")) {
 			return "<p>Hello again</p>";
 		}
 		return "<p>Hello again, " + name + "</p>";
 	}
-	
+
 	@PostMapping("/post")
 	@ResponseBody
-	public String post(@RequestBody(required=false) MultiValueMap<String, Object> cat) {
-		if ( cat == null ) {
+	public String post(@RequestBody(required = false) MultiValueMap<String, Object> cat) {
+		if (cat == null) {
 			return "NG";
 		}
 		System.out.println(cat.getFirst("name"));
